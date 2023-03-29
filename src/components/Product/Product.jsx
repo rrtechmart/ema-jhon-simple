f4f76a383f51b11ee2ecef95c746cbf0ff604dd7
rrtechmart <reza.cpbd@gmail.com>
2023-03-29 06:39:22 +0600

    added all products and info

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const {img, name, price, seller, ratings} = props.product 
+    return (
+        <div className='product'>
+            <img src= {img} alt="" />
+            <div className='product-info'>
+            <h6 className='product-name'> {name} </h6>
+            <p className='product-price'>Price : $ {price}</p>
+            <p className='product-seller'>Manufacturer: {seller} </p>
+            <p className='product-rating'>Rating: {ratings} stars </p>            
+            </div>
+
+            <button className='btn-cart'>Add to Cart </button>
+        </div>
+    );
+};
+
+export default Product;
